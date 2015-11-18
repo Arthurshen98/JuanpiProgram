ef600b442786f0b627ee46e36c2beca1a77468d8
timoteam <shenxuanfei@163.com>
2015-11-18 20:15:53 +0800

    第6次提交

--- app/src/main/java/com/sf/main/juanpiprogram/sf/activity/LoginActivity.java
+++ app/src/main/java/com/sf/main/juanpiprogram/sf/activity/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.sf.main.juanpiprogram.sf.activity;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,10 +11,17 @@ import com.sf.main.juanpiprogram.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    /**
+     *
+     * fragment事物
+     */
+    private FragmentManager fmanager;
+    private FragmentTransaction ftransaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //账号登录
     }
 
     //注册
